c9e346669d4b027563796a4cface96577d5b10c2
hferoze <hassan.feroze@gmail.com>
2015-08-11 13:41:52 -0700

    Tutorial Part 2

--- app/src/main/java/com/funtuts/android/coreinit/animrunner/AnimRunnerActivity.java
+++ app/src/main/java/com/funtuts/android/coreinit/animrunner/AnimRunnerActivity.java
@@ -1,6 +1,13 @@
 package com.funtuts.android.coreinit.animrunner;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class AnimRunnerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_view);
+    }
 }
